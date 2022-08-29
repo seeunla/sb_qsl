cfb96b311342967ef01a1940697a12292e2d149a
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-29 13:18:18 +0900

    세팅

---
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.ll.exam.qsl.user.repository;
+
+import com.ll.exam.qsl.user.entity.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static com.ll.exam.qsl.user.entity.QSiteUser.siteUser;
+
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public SiteUser getQslUser(Long id) {
+        /*
+        SELECT *
+        FROM site_user
+        WHERE id = 1
+        */
+
+        return jpaQueryFactory
+                .select(siteUser)
+                .from(siteUser)
+                .where(siteUser.id.eq(id))
+                .fetchOne();
+    }
+}
