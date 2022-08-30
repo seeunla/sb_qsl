8a40c62e033ea5a09ecc779d62ea5d0e22fb9bfe
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-30 16:14:16 +0900

    @EqualsAndHashCode.Include 도입

--- src/main/java/com/ll/exam/qsl/interesetKeyword/entity/InterestKeyword.java
+++ src/main/java/com/ll/exam/qsl/interesetKeyword/entity/InterestKeyword.java
@@ -3,6 +3,7 @@ package com.ll.exam.qsl.interesetKeyword.entity;
 import com.ll.exam.qsl.user.entity.SiteUser;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,32 +18,12 @@ import java.util.List;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class InterestKeyword {
     @Id
+    @EqualsAndHashCode.Include
     private String content;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        InterestKeyword that = (InterestKeyword) o;
-
-        return content.equals(that.content);
-    }
-
-    @Override
-    public int hashCode() {
-        return content.hashCode();
-    }
-
-    public InterestKeyword(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return content;
-    }
 }
